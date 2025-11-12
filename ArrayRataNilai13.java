d7167f4ee12b2129cf13fa10ff21d963bec3f33a
JVK50 <jidanali32@gmail.com>
2025-11-12 22:24:58 +0700

    Modifikasi no2

--- ArrayRataNilai13.java
+++ ArrayRataNilai13.java
@@ -5,9 +5,13 @@ public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner yesking = new Scanner(System.in);
         
-        int[] nmhs = new int[10];
+        int[] nmhs;
+        double total1 = 0;
         double total = 0;
-        double rata;
+        double rata, rata1;
+
+        System.out.print("Masukkan jumalah mahasiswa: ");
+        nmhs = new int[yesking.nextInt()];
 
         for (int i = 0; i < nmhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +": ");
@@ -15,12 +19,30 @@ public class ArrayRataNilai13 {
         }
         for (int i = 0; i < nmhs.length; i++) {
             if (nmhs[i] > 70) {
-                System.out.println("Mahasiswa ke-"+ (i+1) +" lulus.");
+                total1 += nmhs[i];
+            } else {
+                total += nmhs[i];
+            }
         }
-            total += nmhs[i];
-        }
+        rata1 = total1 / nmhs.length;
         rata = total / nmhs.length;
-        System.out.println("Rata-rata nilai mahasiswa adalah: " + rata);
+        System.out.println("Rata-rata nilai mahasiswa yang lulus adalah: " + rata1);
+        System.out.println("Rata-rata nilai mahasiswa yang tidak lulus adalah: " + rata);
+        
+
+
+        // for (int i = 0; i < nmhs.length; i++) {
+        //     System.out.print("Masukkan nilai mahasiswa ke-"+ (i+1) +": ");
+        //     nmhs[i] = yesking.nextInt();
+        // }
+        // for (int i = 0; i < nmhs.length; i++) {
+        //     if (nmhs[i] > 70) {
+        //         System.out.println("Mahasiswa ke-"+ (i+1) +" lulus.");
+        // }
+        //     total += nmhs[i];
+        // }
+        // rata = total / nmhs.length;
+        // System.out.println("Rata-rata nilai mahasiswa adalah: " + rata);
 
         yesking.close();
     }
