40daabf5cef53d67278ec0a163922605a002b298
JVK50 <jidanali32@gmail.com>
2025-11-15 21:47:25 +0700

    Commit

--- ArrayRataNilai13.java
+++ ArrayRataNilai13.java
@@ -9,6 +9,8 @@ public class ArrayRataNilai13 {
         double total1 = 0;
         double total = 0;
         double rata, rata1;
+        double iya = 0;
+        double tdk = 0;
 
         System.out.print("Masukkan jumalah mahasiswa: ");
         nmhs = new int[yesking.nextInt()];
@@ -20,12 +22,14 @@ public class ArrayRataNilai13 {
         for (int i = 0; i < nmhs.length; i++) {
             if (nmhs[i] > 70) {
                 total1 += nmhs[i];
+                iya++;
             } else {
                 total += nmhs[i];
+                tdk++;
             }
         }
-        rata1 = total1 / nmhs.length;
-        rata = total / nmhs.length;
+        rata1 = total1 / iya;
+        rata = total / tdk;
         System.out.println("Rata-rata nilai mahasiswa yang lulus adalah: " + rata1);
         System.out.println("Rata-rata nilai mahasiswa yang tidak lulus adalah: " + rata);
         
--- Tugas/Tugas1_13.java
+++ Tugas/Tugas1_13.java
@@ -1,5 +1,49 @@
 package Jobsheet9Array.Tugas;
+import java.util.Scanner;
 
 public class Tugas1_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jml[];
+        int total = 0;
+        int rata, i;
+
+        System.out.println("== Program Menghitung dan Menyimpan Nilai Mahasiswa ==");
+        System.out.print("Masukkan jumlah nilai yang akan diinput: ");
+        jml = new int[sc.nextInt()];
+
+        for (i = 0; i < jml.length; i++) {
+            System.out.print("Masukkan Nilai Mahsiswa ke-" + (i+1) +" : ");
+            jml[i] = sc.nextInt();            
+        }
+        for (i = 0; i < jml.length; i++) {
+            System.out.println("Nilai Mahasiswa ke-" + (i+1) + " adalah " + jml[i]);   
+        }
+        for (i = 0; i < jml.length; i++) {
+        total += jml[i];
+        }
     
-}
+        rata = total / jml.length;
+
+        int nilaiTertinggi = jml[0];
+        for (i = 0; i < jml.length; i++) {
+            if (jml[i] > nilaiTertinggi) {
+                nilaiTertinggi = jml[i];
+            }
+        }
+        int nilaiTerendah = jml[0];
+        for (i = 0; i < jml.length; i++) {
+            if (jml[i] < nilaiTerendah) {
+                nilaiTerendah = jml[i];
+            }
+        }
+        System.out.println("== Hasil Analisis ==");
+        System.out.println("Rata-rata nilai mahasiswa adalah " + rata);
+        System.out.println("Nilai tertinggi adalah: " + nilaiTertinggi);
+        System.out.println("Nilai terendah adalah: " + nilaiTerendah);
+        sc.close();        
+    }
+        
+}    
+
