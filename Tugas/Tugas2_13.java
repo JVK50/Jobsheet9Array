6a3ae217cab3e4aac036eaf2da91d8224572a23d
JVK50 <jidanali32@gmail.com>
2025-11-19 09:05:43 +0700

    commit

---
+++ Tugas/Tugas2_13.java
@@ -0,0 +1,13 @@
+package Jobsheet9Array.Tugas;
+import java.util.Scanner;
+
+public class Tugas2_13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+
+        System.out.println("== Pengelola Pesanan Kafe ==");
+        
+    }
+}
