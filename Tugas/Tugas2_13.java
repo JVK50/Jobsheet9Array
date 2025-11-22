de908c5c892e18620a898b8249e61d11f9bc93d6
JVK50 <jidanali32@gmail.com>
2025-11-23 00:55:53 +0700

    Tugas 3

--- Tugas/Tugas2_13.java
+++ Tugas/Tugas2_13.java
@@ -5,43 +5,50 @@ public class Tugas2_13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("== Pengelola Pesanan Kafe ==");
-        System.out.print("Jumlah pesanan: ");
-        int[] pesanan = new int[sc.nextInt()];
+        String[] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"};
+        String key;
+        int index = 0;
+        boolean find = false;
         
-        String[] menu = new String[pesanan.length];
-        double[] harga = new double[pesanan.length];
-        sc.nextLine();
-
-        // Input data pesanan
-        System.out.println("\n--- INPUT DATA PESANAN ---");
-        for (int i = 0; i < pesanan.length; i++) {
-            System.out.print("Masukkan pesanan ke-" + (i+1) + ": ");
-            menu[i] = sc.nextLine();
-
-            System.out.print("Masukkan harga pesanan ke-" + (i+1) + ": Rp ");
-            harga[i] = sc.nextDouble();
-            sc.nextLine();
+        // Menampilkan isi menu
+        System.out.println("== Cafe Pak Garou ==");
+        System.out.println("Daftar Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println("- " + menu[i]);
         }
 
-        // Hitung total biaya
-        double totalBiaya = 0;
-        for (int i = 0; i < harga.length; i++) {
-            totalBiaya += harga[i];
+        // Input pesanan dan kata kunci
+        
+        while (!find) {
+            System.out.println("\nIngin pesan apa? ");
+            System.out.print("- ");
+            key = sc.nextLine();
+            
+            for (int i = 0; i < menu.length; i++) {
+                if (key.equalsIgnoreCase(menu[i])) {
+                    index = i;
+                    find = true;
+                    System.out.println("Menu Tersedia!");
+                    break;
+                }
+            }
+
+            if (!find) {
+                System.out.println("");
+                System.out.println("Menu Tidak Tersedia!");
+                System.out.println("Masukkan Pesanan Lagi!");
+                System.out.println("");
+            }
+
         }
+        
 
         // Tampilkan daftar pesanan
         System.out.println("\n--- DAFTAR PESANAN ---");
-        System.out.println("No. \t Nama Pesanan \t\t Harga");
-        System.out.println("--- \t ------------- \t\t -----");
-        
-        for (int i = 0; i < pesanan.length; i++) {
-            System.out.printf("%d \t %-20s Rp %,.0f\n", (i+1), menu[i], harga[i]);
-        }
-        
-        System.out.println("--- \t ------------- \t\t -----");
-        System.out.printf("TOTAL BIAYA \t\t\t Rp %,.0f\n", totalBiaya);
-        
+        System.out.println("Nama Pesanan");
+        System.out.println("-------------");
+        System.out.println("- " + menu[index]);
+                
         sc.close();
     }
 }
